343cb141c81993d6443ec08aa99d2a2cfbc49f41
Federico Williamson <mhelper123@gmail.com>
2018-08-26 23:35:22 -0300

    Added basic client side code.... calling it a day for now :D

--- public/sketch.js
+++ public/sketch.js
@@ -1,10 +1,18 @@
+var socket;
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	socket = io.connect('http://localhost:3001');
+	socket.on('info',newMessage);
 }
-
+function newMessage(data){
+	//console.log(">",data);
+	background(100,0,100);
+	rect(data.x,data.y,10,10);
+}
+function sendMessage(data){
+	//console.log("<",data);
+	socket.emit('info',data);
+}
 function draw() {
-	if (mouseX>width/2)
-	background(0);
-	else
-	background(255);
+	sendMessage({x:mouseX,y:mouseY});
 }

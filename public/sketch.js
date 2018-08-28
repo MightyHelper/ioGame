0d78d76549b1320cc72a0fa8bfa951d719fc5fb7
Federico Williamson <mhelper123@gmail.com>
2018-08-28 10:00:38 -0300

    Final stage at master

--- public/sketch.js
+++ public/sketch.js
@@ -1,28 +1,57 @@
 var socket;
-var pos
-var spos;
 var players={}
+var useWebGL=false
+function createVec(xpos=0,ypos=0){
+	var x=new p5.Vector();
+	x.set(xpos,ypos);
+	return x
+}
+function setup() {
+	if (useWebGL)createCanvas(windowWidth, windowHeight,WEBGL);
+	else createCanvas(windowWidth, windowHeight);
+	socket = io.connect('http://192.168.0.114:3001');
+	socket.on('player',newMessage);
+	createObstacles()
+}
 function Obstacle(xpos,ypos){
 	this.pos=createVector(xpos,ypos);
 	this.draw=function(){
 		rect(this.pos.x,this.pos.y,10,10);
 	}
 }
-var obstacles=[]
-function  createObstacles(){
-	for (var i=0; i<height; i+=20){
-		for (var o=0; o<width; o+=20){
-			obstacles.push(new Obstacle(i,o))
-		}
-
+function Player(xpos,ypos){
+	if (xpos)
+		this.pos=createVector(xpos,ypos);
+	else
+		this.pos=createVec(0,0);
+	this.vel=createVec(0,0);
+	this.acc=createVec(0,0);
+	this.tick=function(){
+		this.pos.add(this.vel);
+		this.vel.add(this.acc);
+		this.vel.mult(0.9)
+		this.acc.setMag(0);
+	}
+	this.draw=function(){
+		ellipse(this.pos.x,this.pos.y,10,10);
+	}
+	this.applyForce=function(forcex,forcey){
+		this.acc.add(createVec(forcex,forcey));
 	}
 }
-function setup() {
-	createCanvas(windowWidth, windowHeight);
-	socket = io.connect('http://localhost:3001');
-	socket.on('player',newMessage);
-	pos=createVector(0,0);
-	createObstacles()
+function Client(){
+	this.p=new Player()
+	this.draw=function(){
+		this.p.draw()
+	}
+}
+var obstacles=[]
+function  createObstacles(){
+	for (var i=0; i<height; i+=80){
+		for (var o=0; o<width; o+=80){
+			obstacles.push(new Obstacle(i,o))
+		}
+	}
 }
 function newMessage(data){
 	players[data.id]=data.data
@@ -37,28 +66,32 @@ function sendMessage(data,reason='info'){
 	}
 }
 function procKeys(){
-	if (keyIsDown(LEFT_ARROW))pos.x -= 5;
-	if (keyIsDown(RIGHT_ARROW))pos.x += 5;
-	if (keyIsDown(UP_ARROW))pos.y -= 5;
-	if (keyIsDown(DOWN_ARROW))pos.y += 5;
+	if (keyIsDown(LEFT_ARROW))client.p.applyForce(-1,0);
+	if (keyIsDown(RIGHT_ARROW))client.p.applyForce(1,0);
+	if (keyIsDown(UP_ARROW))client.p.applyForce(0,-1);
+	if (keyIsDown(DOWN_ARROW))client.p.applyForce(0,1);
 }
 function drawPlayers(){
 	for (player in players)
-		ellipse(players[player].x,players[player].y,10,10);
+		ellipse(players[player].p.pos.x,players[player].p.pos.y,10,10);
 }
 function drawObstacles(){
 	for (o in obstacles){
 		obstacles[o].draw()
 	}
 }
+var client=new Client()
 function draw() {
-	sendMessage(pos,'player');
+
+	background(255);
+	client.p.tick()
+	sendMessage(client,'player');
 	clear()
 	procKeys()
-	translate(width/2,height/2);
-	translate(-pos.x,-pos.y);
+	if (!useWebGL)translate(width/2,height/2);
+	translate(-client.p.pos.x,-client.p.pos.y)
 	fill(255);
-	ellipse(pos.x,pos.y,10,10)
+	client.draw()
 	drawObstacles()
 	drawPlayers()
 }
